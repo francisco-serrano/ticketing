2e219c95fe356f2c0245fbcf97d0ce74890c9c19
francisco.serrano <francisco.serrano372@gmail.com>
2020-09-06 03:38:44 -0300

    adds fixes for ingress

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
     if (typeof window === 'undefined') {
         // we are on the server
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.metegol-app.com/',
             headers: req.headers,
         });
     } else {
